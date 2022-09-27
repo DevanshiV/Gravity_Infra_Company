00d785c91a5376549482bff9a589fb8e01bec3ec
Devanshi <devanshivarshney2@gmail.com>
2022-09-27 17:27:21 +0530

    second commit(first by devidevivdevi)

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=01e22c981be2bfcae80a95876eee8820", function(data) {
+    console.log(data);
+    var temp = data.main.temp;
+    $(".temp").append(temp);
+    var icon = data.weather[0].icon;
+    var det = data.weather[0].description;
+    url = "http://openweathermap.org/img/wn/" + icon + ".png ";
+
+    $('.icon').attr('src', url);
+    $(".details").append(det);
+
+})
